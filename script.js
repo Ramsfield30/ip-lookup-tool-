dc10450b96e045f509db02659b1d698b077357a1
Ramsfield30 <ayibakeperemo476@gmail.com>
2026-02-10 12:36:05 +0100

    update script.js

--- script.js
+++ script.js
@@ -11,14 +11,14 @@ searchBtn.addEventListener('click', function() {
     
     fetch(`https://ipapi.co/${ipAddress}/json/`)
         .then(response => response.json())
-    .then(data => {
-    document.getElementById('network').textContent = data.org || 'Not available';
-    document.getElementById('city').textContent = data.city || 'Not available';
-    document.getElementById('country').textContent = data.country_name || 'Not available';
-    document.getElementById('timezone').textContent = data.timezone || 'Not available';
-    document.getElementById('coords').textContent = data.latitude && data.longitude ? `${data.latitude}, ${data.longitude}` : 'Not available';
-    document.getElementById('zip').textContent = data.postal || 'Not available';
-})    
+        .then(data => {
+            document.getElementById('network').textContent = data.org || 'Not available';
+            document.getElementById('city').textContent = data.city || 'Not available';
+            document.getElementById('country').textContent = data.country_name || 'Not available';
+            document.getElementById('timezone').textContent = data.timezone || 'Not available';
+            document.getElementById('coords').textContent = data.latitude && data.longitude ? `${data.latitude}, ${data.longitude}` : 'Not available';
+            document.getElementById('zip').textContent = data.postal || 'Not available';
+        })
         .catch(error => {
             alert('Error: ' + error);
         });
